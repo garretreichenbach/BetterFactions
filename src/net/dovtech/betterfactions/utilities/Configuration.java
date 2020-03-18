7a8ac2890b790d36efe6757ae07e78246f5414cf
TheDerpGamer <garret2kool@gmail.com>
2020-03-18 11:02:56 -0700

    Create Configuration.java

---
+++ src/net/dovtech/betterfactions/utilities/Configuration.java
@@ -0,0 +1,30 @@
+package net.dovtech.betterfactions.utilities;
+
+public class Configuration {
+
+    /* BetterFactionsConfig.yml
+    # BetterFactions Config File
+    #
+    # Factions
+    #
+    # Stations
+    # Mining Stations
+    miningLevel1Multiplier: 1.3
+    miningLevel2Multiplier: 1.65
+    miningLevel3Multiplier: 2
+    miningLevel4Multiplier: 2.5
+    miningLevel5Multiplier: 3
+    resourceZoneMaxSize: 10
+    resourceZoneMinSize: 5
+    resourceZoneDensity: 5
+     */
+
+    private double miningLevel1Multiplier;
+    private double miningLevel2Multiplier;
+    private double miningLevel3Multiplier;
+    private double miningLevel4Multiplier;
+    private double miningLevel5Multiplier;
+    private int resourceZoneMaxSize;
+    private int resourceZoneMinSize;
+    private int resourceZoneDensity;
+}
