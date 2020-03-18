b067027c77dafa9345d02b1b131beaa850c19b3f
TheDerpGamer <garret2kool@gmail.com>
2020-03-18 12:03:01 -0700

    Update ConfigReader.java

--- src/net/dovtech/betterfactions/utilities/ConfigReader.java
+++ src/net/dovtech/betterfactions/utilities/ConfigReader.java
@@ -1,16 +1,17 @@
 package net.dovtech.betterfactions.utilities;
 
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import java.io.InputStream;
 
 public class ConfigReader {
 
-    private Yaml config = new Yaml();
+    private static String configPath = "/SMMods/BetterFactions/config.yml";
+    private static Yaml config = new Yaml(new Constructor(Configuration.class));
 
-    public Yaml getConfig() {
-
-    }
-
-    public static double getMiningLevelMultiplier(int level) {
-
+    public static Configuration getConfig() {
+        InputStream inputStream = ConfigReader.class.getClassLoader().getResourceAsStream(configPath);
+        Configuration configuration = config.load(inputStream);
+        return configuration;
     }
 }
