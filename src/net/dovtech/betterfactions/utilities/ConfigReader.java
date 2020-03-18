e6d2c3666748880d2ebe9ab30a1cccae4782b2fe
TheDerpGamer <garret2kool@gmail.com>
2020-03-18 11:02:49 -0700

    Create ConfigReader.java

---
+++ src/net/dovtech/betterfactions/utilities/ConfigReader.java
@@ -0,0 +1,16 @@
+package net.dovtech.betterfactions.utilities;
+
+import org.yaml.snakeyaml.Yaml;
+
+public class ConfigReader {
+
+    private Yaml config = new Yaml();
+
+    public Yaml getConfig() {
+
+    }
+
+    public static double getMiningLevelMultiplier(int level) {
+
+    }
+}
