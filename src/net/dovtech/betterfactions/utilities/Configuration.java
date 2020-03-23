8b59e942499a965b11086f13a7e21c405f69fae8
TheDerpGamer <garret2kool@gmail.com>
2020-03-23 12:34:04 -0700

    Update Configuration.java

--- src/net/dovtech/betterfactions/utilities/Configuration.java
+++ src/net/dovtech/betterfactions/utilities/Configuration.java
@@ -10,11 +10,11 @@ public class Configuration {
     #
     # Stations
     # Mining Stations
-    miningLevel1Multiplier: 1.025
-    miningLevel2Multiplier: 1.05
-    miningLevel3Multiplier: 1.07
-    miningLevel4Multiplier: 1.1
-    miningLevel5Multiplier: 1.15
+    gasMiningLevel1Multiplier: 1.025
+    gasMiningLevel2Multiplier: 1.05
+    gasMiningLevel3Multiplier: 1.07
+    gasMiningLevel4Multiplier: 1.1
+    gasMiningLevel5Multiplier: 1.15
     resourceZoneMaxSize: 10
     resourceZoneMinSize: 5
     resourceZoneDensity: 5
@@ -23,11 +23,11 @@ public class Configuration {
     resourceZoneRegenMultiplier: 0.007
      */
 
-    private double miningLevel1Multiplier;
-    private double miningLevel2Multiplier;
-    private double miningLevel3Multiplier;
-    private double miningLevel4Multiplier;
-    private double miningLevel5Multiplier;
+    private double gasMiningLevel1Multiplier;
+    private double gasMiningLevel2Multiplier;
+    private double gasMiningLevel3Multiplier;
+    private double gasMiningLevel4Multiplier;
+    private double gasMiningLevel5Multiplier;
     private int resourceZoneMaxSize;
     private int resourceZoneMinSize;
     private int resourceZoneDensity;
@@ -35,24 +35,24 @@ public class Configuration {
     private int resourceZoneMinCap;
     private double resourceZoneRegenMultiplier;
 
-    public double getMiningLevel1Multiplier() {
-        return miningLevel1Multiplier;
+    public double getGasMiningLevel1Multiplier() {
+        return gasMiningLevel1Multiplier;
     }
 
-    public double getMiningLevel2Multiplier() {
-        return miningLevel2Multiplier;
+    public double getGasMiningLevel2Multiplier() {
+        return gasMiningLevel2Multiplier;
     }
 
-    public double getMiningLevel3Multiplier() {
-        return miningLevel3Multiplier;
+    public double getGasMiningLevel3Multiplier() {
+        return gasMiningLevel3Multiplier;
     }
 
-    public double getMiningLevel4Multiplier() {
-        return miningLevel4Multiplier;
+    public double getGasMiningLevel4Multiplier() {
+        return gasMiningLevel4Multiplier;
     }
 
-    public double getMiningLevel5Multiplier() {
-        return miningLevel5Multiplier;
+    public double getGasMiningLevel5Multiplier() {
+        return gasMiningLevel5Multiplier;
     }
 
     public int getResourceZoneMaxSize() {
