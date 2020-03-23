6fd83b0f3a89122dab5881940f48d75ce9957c80
TheDerpGamer <garret2kool@gmail.com>
2020-03-23 10:51:14 -0700

    Create ResourceManager.java

---
+++ src/net/dovtech/betterfactions/element/ResourceManager.java
@@ -0,0 +1,10 @@
+package net.dovtech.betterfactions.element;
+
+import api.element.block.Block;
+
+public class ResourceManager extends Block {
+    public ResourceManager() {
+        super(3000, "RESOURCE_MANAGER");
+        this.setFullName("Resource Manager");
+    }
+}
