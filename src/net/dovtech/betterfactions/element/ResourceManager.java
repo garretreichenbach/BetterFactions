3733f77f86d1c8d325e87069921326d129c7bb9d
TheDerpGamer <garret2kool@gmail.com>
2020-03-30 09:44:53 -0700

    Update ResourceManager.java

--- src/net/dovtech/betterfactions/element/ResourceManager.java
+++ src/net/dovtech/betterfactions/element/ResourceManager.java
@@ -1,10 +1,20 @@
 package net.dovtech.betterfactions.element;
 
-import api.element.block.Block;
+import api.inventory.ItemStack;
+import net.dovtech.betterfactions.BetterFactions;
+import org.schema.game.common.data.element.ElementInformation;
 
-public class ResourceManager extends Block {
-    public ResourceManager() {
-        super(3000, "RESOURCE_MANAGER");
-        this.setFullName("Resource Manager");
+public class ResourceManager extends ItemStack {
+
+    private ElementInformation blockInfo = getInfo();
+
+    public ResourceManager(short id) {
+        super(id);
+        blockInfo.setCanActivate(true);
+        BetterFactions.blocks.add(this);
+    }
+
+    public void onActivate() {
+
     }
 }
