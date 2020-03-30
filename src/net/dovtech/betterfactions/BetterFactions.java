49fa26fef4831e2e491329e666b65e6139e090fa
TheDerpGamer <garret2kool@gmail.com>
2020-03-30 09:44:46 -0700

    Update BetterFactions.java

--- src/net/dovtech/betterfactions/BetterFactions.java
+++ src/net/dovtech/betterfactions/BetterFactions.java
@@ -1,18 +1,29 @@
 package net.dovtech.betterfactions;
 
 import api.DebugFile;
+import api.config.BlockConfig;
+import api.inventory.ItemStack;
 import api.mod.StarMod;
+import java.util.List;
 
 public class BetterFactions extends StarMod {
     static BetterFactions inst;
     public BetterFactions() {
         inst = this;
     }
+    public static List<ItemStack> blocks;
 
     @Override
     public void onEnable() {
         super.onEnable();
         this.modName = "BetterFactions";
         DebugFile.log("Enabled", this);
+    }
+
+    @Override
+    public void onBlockConfigLoad(BlockConfig config) {
+        for(ItemStack block : blocks) {
+            config.add(block.getInfo());
+        }
     }
 }
