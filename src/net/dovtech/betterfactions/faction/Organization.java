88f331133ee9c51b78c594d6c91eb21113a4cf9a
TheDerpGamer <garret2kool@gmail.com>
2020-05-27 13:45:52 -0700

    Create Organization.java

---
+++ src/net/dovtech/betterfactions/faction/Organization.java
@@ -0,0 +1,10 @@
+package net.dovtech.betterfactions.faction;
+
+import api.faction.Faction;
+import java.util.ArrayList;
+
+public class Organization {
+
+    private ArrayList<Faction> memberFactions;
+    private String name;
+}
