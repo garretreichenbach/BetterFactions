5bb7b4315ba1cb78515fbb6434b342aef26f01ba
TheDerpGamer <garret2kool@gmail.com>
2020-05-31 17:09:04 -0700

    Update NewFactionPanel.java

--- src/net/dovtech/betterfactions/gui/NewFactionPanel.java
+++ src/net/dovtech/betterfactions/gui/NewFactionPanel.java
@@ -1,7 +1,7 @@
 package net.dovtech.betterfactions.gui;
 
 import org.schema.game.client.view.gui.faction.newfaction.FactionPanelNew;
-import org.schema.schine.graphicsengine.forms.gui.GUIListElement;
+import org.schema.schine.graphicsengine.forms.gui.*;
 import org.schema.schine.graphicsengine.forms.gui.newgui.GUIContentPane;
 import org.schema.schine.graphicsengine.forms.gui.newgui.GUIContextPane;
 import org.schema.schine.graphicsengine.forms.gui.newgui.GUITabbedContent;
@@ -9,6 +9,12 @@ import org.schema.schine.input.InputState;
 
 public class NewFactionPanel extends FactionPanelNew {
 
+    private GUIContentPane newsTab;
+    private GUIContentPane factionRelations;
+    private GUIContentPane factionNews;
+    private GUIContentPane members;
+    private GUIContentPane factionList;
+    private GUIContentPane options;
     private GUIContentPane organization;
 
     public NewFactionPanel(InputState inputState) {
@@ -18,18 +24,49 @@ public class NewFactionPanel extends FactionPanelNew {
     @Override
     public void recreateTabs() {
         super.recreateTabs();
-        organization = this.factionPanel.addTab("ORGANIZATION");
-        createOrgPanel();
+        getTabs();
+        modifyFactionRelations();
+        modifyFactionList();
+        //createOrgPanel();
+    }
+
+    private void getTabs() {
+        for(GUIContentPane tab : factionPanel.getTabs()) {
+            if(tab.getTabName().toString().contains("NEWS")) {
+                newsTab = tab;
+            } else if(tab.getTabName().toString().contains("NPC DIPLOMACY")) {
+                factionRelations = tab;
+            } else if(tab.getTabName().toString().contains("FACTION NEWS")) {
+                factionNews = tab;
+            } else if(tab.getTabName().toString().contains("MEMBERS")) {
+                members = tab;
+            } else if(tab.getTabName().toString().contains("LIST")) {
+                factionList = tab;
+            } else if(tab.getTabName().toString().contains("OPTIONS")) {
+                options = tab;
+            }
+        }
+    }
+
+    private void modifyFactionRelations() {
+        factionRelations.setTabName("FACTION RELATIONS");
+    }
+
+    private void modifyFactionList() {
+        factionList.setTabName("FACTION LIST");
     }
 
     private void createOrgPanel() {
+
+        organization = this.factionPanel.addTab("ORGANIZATION");
+
         organization.setName("ORGANIZATION");
-        
+
         GUITabbedContent organizationPanel = new GUITabbedContent(organization.getState(), organization);
 
         //Organization Info
         GUIContextPane orgInfo = new GUIContextPane(organization.getState(), 4.0F, 4.0F);
-        GUIListElement memberFactions = new GUIListElement(organization.getState());
+        GUIElementList memberFactions = new GUIElementList(organization.getState());
 
         //Organization News
         GUIContentPane organizationNews = new GUIContentPane(organizationPanel.getState(), organizationPanel, "ORGANIZATION NEWS");
