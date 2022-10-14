519c992f72efc78c25c5f8b954079b603b6fa52e
Garret Reichenbach <garretreichenbach@gmail.com>
2022-10-14 12:36:45 -0700

    Update EventManager.java

--- src/main/java/thederpgamer/betterfactions/manager/EventManager.java
+++ src/main/java/thederpgamer/betterfactions/manager/EventManager.java
@@ -1,6 +1,13 @@
 package thederpgamer.betterfactions.manager;
 
+import api.listener.Listener;
+import api.listener.events.gui.PlayerGUICreateEvent;
+import api.mod.StarLoader;
+import org.schema.game.client.view.gui.PlayerPanel;
 import thederpgamer.betterfactions.BetterFactions;
+import thederpgamer.betterfactions.gui.factionpanel.BFFactionPanelNew;
+
+import java.lang.reflect.Field;
 
 /**
  * [Description]
@@ -10,5 +17,22 @@ import thederpgamer.betterfactions.BetterFactions;
 public class EventManager {
 
 	public static void registerEvents(BetterFactions betterFactions) {
+		StarLoader.registerListener(PlayerGUICreateEvent.class, new Listener<PlayerGUICreateEvent>() {
+			@Override
+			public void onEvent(PlayerGUICreateEvent event) {
+				try {
+					PlayerPanel playerPanel = event.getPlayerPanel();
+					Field factionPanelNewField = playerPanel.getClass().getDeclaredField("factionPanelNew");
+					factionPanelNewField.setAccessible(true);
+					if(!(factionPanelNewField.get(playerPanel) instanceof BFFactionPanelNew)) {
+						BFFactionPanelNew factionPanelNew = new BFFactionPanelNew(playerPanel.getState());
+						factionPanelNew.onInit();
+						factionPanelNewField.set(playerPanel, factionPanelNew);
+					}
+				} catch (NoSuchFieldException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
+		}, betterFactions);
 	}
 }
